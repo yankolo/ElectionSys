3369bc102ef434d561e08de77d8167d7e2526731
chaoukis <sammy.chaouki@dawsoncollege.qc.ca>
2017-10-26 11:14:28 -0400

    Import Stream

--- ElectionSys/src/election/data/SortMergeApp.java
+++ ElectionSys/src/election/data/SortMergeApp.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 import election.business.interfaces.Election;
 import election.business.interfaces.Tally;
