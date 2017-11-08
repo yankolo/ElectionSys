f3cf83e97831a1b0f57e7075d57f5da3c3fd29cb
chaoukis <sammy.chaouki@dawsoncollege.qc.ca>
2017-11-07 21:45:36 -0500

    Update DuplicateVoterException

--- ElectionSys/src/election/data/DuplicateVoterException.java
+++ ElectionSys/src/election/data/DuplicateVoterException.java
@@ -10,7 +10,7 @@ public class DuplicateVoterException extends Exception {
 	private static final long  serialVersionUID = 42031768871L;
 	
 	public DuplicateVoterException(){
-		super("The provided Email address is associated with an existing voter.");
+		super("The provided Email address is associated with an already existing voter in the database.");
 	}
 
 	public DuplicateVoterException(String message) {
