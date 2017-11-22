dce508608aa03db89ab4be44e2508d95156e13ac
Yanik Kolomatski <yankolo1234@gmail.com>
2017-11-22 14:49:51 -0500

    Add ObjectSerializedList Class

---
+++ ElectionSys/src/election/data/ObjectSerializedList.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package election.data;
+
+import java.io.IOException;
+import java.util.List;
+
+import election.business.interfaces.Election;
+import election.business.interfaces.Voter;
+import election.data.interfaces.ListPersistenceObject;
+
+/**
+ * @author Yanik, Mohamed
+ *
+ */
+public class ObjectSerializedList implements ListPersistenceObject {
+
+	@Override
+	public List<Voter> getVoterDatabase() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Election> getElectionDatabase() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveVoterDatabase(List<Voter> voters) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void saveElectionDatabase(List<Election> elections) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
