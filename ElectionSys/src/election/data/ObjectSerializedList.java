8e0130d454940a292eac37e78bf4782ee9501010
Mohamed Hamza <mohamed.hamza@dawsoncollege.qc.ca>
2017-11-26 16:47:16 -0500

    Add Javadoc

--- ElectionSys/src/election/data/ObjectSerializedList.java
+++ ElectionSys/src/election/data/ObjectSerializedList.java
@@ -14,8 +14,11 @@ import election.business.interfaces.Voter;
 import election.data.interfaces.ListPersistenceObject;
 
 /**
- * @author Yanik, Mohamed
- *
+ * @author Yanik, Mohamed 
+ *         
+ *         ObjectSerializedList interacts with object serialized
+ *         files. It allow the election system to retrieve/save the database
+ *         from/to object serialized files.
  */
 public class ObjectSerializedList implements ListPersistenceObject {
 
@@ -27,37 +30,57 @@ public class ObjectSerializedList implements ListPersistenceObject {
 		this.electionFilename = electionFilename;
 	}
 
+	/**
+	 * It creates an empty Voter List. Voters are added to the empty List using the
+	 * deserializeObject method from the Utilities class. If an exception occurs
+	 * while deserializing, an error message is printed and the Voters will not be
+	 * added into the List.
+	 */
 	@Override
 	public List<Voter> getVoterDatabase() {
 		List<Voter> voters = new ArrayList<>();
-		
+
 		try {
 			voters = (List<Voter>) util.Utilities.deserializeObject(voterFilename);
 		} catch (IOException | ClassNotFoundException e) {
 			System.err.println(e.getMessage());
-		} 
-		
+		}
+
 		return voters;
 	}
 
+	/**
+	 * It creates an empty Election List. Elections are added to the empty List
+	 * using the deserializeObject method from the Utilities class. If an exception
+	 * occurs while deserializing, an error message is printed and the Elections
+	 * will not be added into the List.
+	 */
 	@Override
 	public List<Election> getElectionDatabase() {
 		List<Election> elections = new ArrayList<>();
-		
+
 		try {
 			elections = (List<Election>) util.Utilities.deserializeObject(electionFilename);
 		} catch (IOException | ClassNotFoundException e) {
 			System.err.println(e.getMessage());
-		} 
-		
+		}
+
 		return elections;
 	}
 
+	/**
+	 * It serializes the List of Voters using the serializedObject method from the
+	 * Utilities class.
+	 */
 	@Override
 	public void saveVoterDatabase(List<Voter> voters) throws IOException {
 		util.Utilities.serializeObject(voters, voterFilename);
 	}
 
+	/**
+	 * It serializes the List of Elections using the serializedObject method from
+	 * the utilities class.
+	 */
 	@Override
 	public void saveElectionDatabase(List<Election> elections) throws IOException {
 		util.Utilities.serializeObject(elections, electionFilename);
