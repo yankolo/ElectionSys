cf6acef20d768611d942f774d98931a90f26f38d
Yanik Kolomatski <yanik.kolomatski@dawsoncollege.qc.ca>
2017-12-08 19:54:05 -0500

    Add GUI

--- ElectionSys/src/election/ui/gui/VoterEmailFormGUI.java
+++ ElectionSys/src/election/ui/gui/VoterEmailFormGUI.java
@@ -3,6 +3,7 @@ package election.ui.gui;
 import election.business.DawsonElectionOffice;
 import election.business.ElectionType;
 import election.business.interfaces.*;
+import election.data.InexistentVoterException;
 import javafx.application.Application;
 import javafx.beans.binding.Bindings;
 import javafx.event.ActionEvent;
@@ -21,6 +22,10 @@ public class VoterEmailFormGUI {
 	
 	private ElectionOffice model;
 	private Election election;
+	private Stage primaryStage;
+	
+	private TextField txtField;
+	private Label errorMsg;
 	
 //TODO add any additional properties
 	
@@ -31,7 +36,17 @@ public class VoterEmailFormGUI {
 	 *  @throws IllegalArgumentException if the conditions are not met.
 	 */
     public VoterEmailFormGUI(ElectionOffice model, Election election) {
-		//TODO
+		if(model != null && election != null) {
+			if(election.getElectionType().equals(ElectionType.SINGLE)){
+				this.model = model;
+				this.election = election;
+				initialize();
+			} else {
+				throw new IllegalArgumentException();
+			}
+		} else {
+			throw new IllegalArgumentException();
+		}
     }
 
     /**
@@ -55,7 +70,31 @@ public class VoterEmailFormGUI {
      * @return GridPane with the UI
      */
     private GridPane createUserInterface() {
-        // TODO
+    	GridPane grid = new GridPane();
+    	
+        RowConstraints row1 = new RowConstraints();
+        RowConstraints row2 = new RowConstraints();
+        RowConstraints row3 = new RowConstraints();
+        row1.setPercentHeight(25);
+        row2.setPercentHeight(25);
+        row3.setPercentHeight(25);
+        grid.getRowConstraints().addAll(row1,row2,row3);
+        
+
+        grid.setPadding(new Insets(25, 25, 25, 25));
+        
+        Label label = new Label("Enter email:");
+        txtField = new TextField();
+        Button button = new Button("Sign In");
+        button.setOnAction(this::signInButtonHandler);
+        errorMsg = new Label();
+        
+        grid.add(label, 0, 0);
+        grid.add(txtField, 0, 1);
+        grid.add(button, 0, 2);
+        grid.add(errorMsg, 0, 3);
+        
+        return grid;
     }
 
     /**
@@ -64,8 +103,22 @@ public class VoterEmailFormGUI {
      * @param e
      */
     private void signInButtonHandler(ActionEvent e) {
- 
-		//TODO
+    	try {
+    		Voter voter = model.findVoter(txtField.getText());
+    		
+    		if (voter.isEligible(election)) {
+    			Ballot ballot = model.getBallot(voter, election);
+    			SingleBallotFormGUI form = new SingleBallotFormGUI(model, election, voter, ballot, this);
+    			form.start(primaryStage);
+    		} else {
+    			errorMsg.setText("The voter is not elligble");
+    		}
+    	} catch (InexistentVoterException ive) {
+    		errorMsg.setText("The voter doesn't exist in the database");
+    	} catch (Exception expection) {
+    		errorMsg.setText(expection.getMessage());
+    	}
+		
     }
 
     /**
@@ -76,6 +129,6 @@ public class VoterEmailFormGUI {
      * the bean is immediately shown in the control.
      */
     private void initialize() {
-
+    	
     }
 }
